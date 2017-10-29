6954e93da0a96c8f55e7d3b431953b1101eda66a
Durim Kryeziu <durimkryeziu7@gmail.com>
2017-10-29 23:34:43 +0100

    Refactor and code clean up

--- src/main/java/org/prnhs/javaee/swim/config/ProfilerConfig.java
+++ src/main/java/org/prnhs/javaee/swim/config/ProfilerConfig.java
@@ -2,26 +2,25 @@ package org.prnhs.javaee.swim.config;
 
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
-import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ProfilerConfig {
 
-    static final MetricRegistry metrics = new MetricRegistry();
-    
-    @Bean
-    public MetricRegistry metrics(){
-        return metrics;
-    }
-    
-    @Bean
-    public ConsoleReporter reporter(){
-        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
-                .convertRatesTo(TimeUnit.SECONDS)
-                .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .build();
-        return reporter;
-    }
+  @Bean
+  public MetricRegistry metrics() {
+    return new MetricRegistry();
+  }
+
+  @Bean
+  public ConsoleReporter reporter(MetricRegistry metrics) {
+    ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+        .convertRatesTo(TimeUnit.SECONDS)
+        .convertDurationsTo(TimeUnit.MILLISECONDS)
+        .build();
+    return reporter;
+  }
 }
